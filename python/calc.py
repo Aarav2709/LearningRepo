d3d4ad89e1795ee17d9ab75a5494cca218b88968
Aarav Gupta <tribejustice35@gmail.com>
2025-12-20 23:48:40 +0530

    v2 : More Advanced?!

--- python/calc.py
+++ python/calc.py
@@ -1,7 +1,10 @@
 # Entering values!
-num1 = int(input("Enter the first number: "))
-num2 = int(input("Enter the second number: "))
-ans = num1 + num2
+def main():
+    num1 = int(input("Enter the first number: "))
+    num2 = int(input("Enter the second number: "))
+    ans = float(num1 + num2)
+    print(f"The sum of the numbers are {ans:,}!")
+
 
 # Outputting!
-print(f"{ans:,}")
+main()
