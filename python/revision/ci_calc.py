1a18f1daa3639733997c174f48b68c981b01addf
Aarav Gupta <tribejustice35@gmail.com>
2026-07-14 00:05:52 +0530

    CI Calculator V2.

--- python/revision/ci_calc.py
+++ python/revision/ci_calc.py
@@ -1,22 +1,28 @@
-# init the vars.
+# commenting on this file cause I feel like it.
 p = 0
 r = 0
 t = 0
 
-while p <= 0: # wait for user's input.
+while True: # wait for user's input.
   p = float(input("Enter the principle: "))
   if p <= 0:
     print("Principle can't be zero. Try again!")
+  else:
+    break
 
-while r <= 0: # wait for user's input.
+while True: # wait for user's input.
   r = float(input("Enter the rate: "))
   if r <= 0:
     print("Rate can't be zero. Try again!")
+  else:
+    break
 
-while t <= 0: # wait for user's input.
+while True: # wait for user's input.
   t = float(input("Enter the time: "))
   if t <= 0:
     print("Time can't be zero. Try again!")
+  else:
+    break
 
 ci = p * pow((1 + r / 100), t) # calculate the compound interest.
 
